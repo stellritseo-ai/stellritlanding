59f30d1ca73b5568a5bd668da23360a0ad390a17
Jiten Sony <jitenksony@gmail.com>
2026-08-03 11:54:38 +0545

    feat(gtm): update Google Tag Manager container ID to GTM-NVSV2BZN

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -382,6 +382,15 @@ function RootShell({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NVSV2BZN');`,
+          }}
+        />
         <HeadContent />
         <script
           type="application/ld+json"
@@ -389,6 +398,12 @@ function RootShell({ children }: { children: ReactNode }) {
         />
       </head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NVSV2BZN"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
         {children}
         <Scripts />
       </body>
