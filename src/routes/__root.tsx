d787588a6daff13dce37597eab42c2800063899c
Jiten Sony <jitenksony@gmail.com>
2026-08-03 11:24:25 +0545

    feat(gtm): add Google Tag Manager container GTM-T3FZML9C

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -383,6 +383,15 @@ function RootShell({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T3FZML9C');`,
+          }}
+        />
         <HeadContent />
         <script
           type="application/ld+json"
@@ -390,6 +399,12 @@ function RootShell({ children }: { children: ReactNode }) {
         />
       </head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T3FZML9C"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
         {children}
         <Scripts />
       </body>
